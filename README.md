79fb5ba4424cafb2c2ca170b177670f7f22591c5
chasestanley000 <chasestanley000@gmail.com>
2019-03-05 18:14:01 -0600

    updated project structure; added to readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Spiderbot
+
+This repository functions as a continuously tracked  code base for the operation of
+the capstone project "Spiderbot: Hexapedal Search Robot".
--- src/spiderbot.py
+++ src/spiderbot.py
@@ -5,8 +5,12 @@ from time import sleep
 def main():
     front_right_horizontal = legJoint( gpio_pin=21 )
     front_right_vertical = legJoint( gpio_pin=20 )
-
-    move_forward( front_right_horizontal, front_right_vertical, 'right')
+    
+    for _ in range(5):
+        move_forward( front_right_horizontal, front_right_vertical, 'right')
+        sleep(0.1)
+        front_right_horizontal.center_joint()
+        sleep(0.1)
 
     exit(0)
 
@@ -14,11 +18,15 @@ def  move_forward( joint1, joint2, orientation):
     
     if orientation == 'right':
         joint2.rotate_counter_clockwise()
+        sleep(0.1)
         joint1.rotate_counter_clockwise()
+        sleep(0.1)
         joint2.center_joint()
     elif orientation == 'left':
         joint2.rotate_clockwise()
+        sleep(0.1)
         joint1.rotate_clockwise()
+        sleep(0.1)
         joint2.center_joint()
     else:
         raise ValueError 
